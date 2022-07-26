e80a209ccc2b46c1604db2ce275900c7b3d92839
mwalo254 <mwalonick@gmail.com>
2022-07-26 09:05:03 +0300

    updates the modal changes

--- src/components/TestimonialsDialog.vue
+++ src/components/TestimonialsDialog.vue
@@ -12,7 +12,7 @@
   <div class="modal-dialog">
     <div class="modal-content">
       <div class="modal-header">
-        <h5 class="modal-title" id="exampleModalLabel">Edit</h5>
+        <h5 class="modal-title" id="exampleModalLabel">Edit Testimonial</h5>
         <button type="button" class="close" data-dismiss="modal" aria-label="Close">
           <span aria-hidden="true">&times;</span>
         </button>
@@ -22,7 +22,7 @@
       </div>
       <div class="modal-footer">
         <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-        <button type="button" class="btn btn-primary">Save changes</button>
+        <button type="button" class="btn btn-primary">Update</button>
       </div>
     </div>
   </div>
