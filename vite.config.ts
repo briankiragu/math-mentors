880ecc91f21ba4f48b10ff3d1bfdc78fba776170
Brian Kariuki <bkariuki@hotmail.com>
2022-08-31 09:41:23 +0300

    Changed package.json

--- vite.config.ts
+++ vite.config.ts
@@ -1,22 +1,17 @@
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
-import path from 'path';
+
+const path = require('path');
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
   build: {
     sourcemap: true,
-    // outDir: path.resolve(__dirname, './dist'),
-    rollupOptions: {
-      output: {
-        // file: 'testimonials.js',
-        dir: path.resolve(__dirname, './dist'),
-      },
-    },
+    outDir: path.resolve(__dirname, './dist'),
   },
   resolve: {
-    extensions: ['.json', '.mjs', '.js', '.ts', '.jsx', '.tsx', '.vue'],
+    extensions: ['.json', '.js', '.mjs', '.ts', '.jsx', '.tsx', '.vue'],
     alias: {
       vue: 'vue/dist/vue.esm-bundler.js',
       '@': path.resolve(__dirname, './src'),
