85740db66a276765dad352c7e348eeeb849d6c4f
Brian Kariuki <bkariuki@hotmail.com>
2022-09-06 10:18:24 +0300

    Dialog mobile design

--- src/components/TestimonialsDialog.vue
+++ src/components/TestimonialsDialog.vue
@@ -1,13 +1,20 @@
 <template>
   <div>
     <!-- Dialog trigger -->
-    <button type="button" @click.prevent="showDialog">Edit</button>
+    <button type="button" class="dialog-trigger" @click.prevent="showDialog">
+      Edit
+    </button>
 
     <!-- Dialog content -->
-    <dialog id="testimonials" ref="dialogEl" modal-mode="mega">
+    <dialog
+      id="testimonials"
+      ref="dialogEl"
+      class="dialog-content"
+      modal-mode="mega"
+    >
       <form method="dialog">
         <!-- Dialog header -->
-        <header class="dialog__header">
+        <header class="dialog-content__header">
           <h3>Update Testimonials</h3>
           <button @click.prevent="closeDialog('close')">
             <img
@@ -20,17 +27,19 @@
         </header>
 
         <!-- Dialog content -->
-        <article class="dialog__content">
+        <article class="dialog-content__body">
           <!-- Tab headings -->
-          <nav class="mb-2">
+          <nav class="dialog-content__body__nav">
             <button
-              class="btn btn-primary mr-1"
+              class="dialog-content__body__nav-item"
+              :class="{ active: activeTab === 'HTML' }"
               @click.prevent="handleTabClick('HTML')"
             >
               HTML
             </button>
             <button
-              class="btn btn-secondary"
+              class="dialog-content__body__nav-item"
+              :class="{ active: activeTab === 'JSON' }"
               @click.prevent="handleTabClick('JSON')"
             >
               JSON
@@ -40,11 +49,11 @@
           <!-- Tab body -->
           <main>
             <TestimonialsDialogFormTab
-              v-show="currentActiveTab === 'HTML'"
+              v-show="activeTab === 'HTML'"
               v-model:testimonials="testimonials"
             />
             <TestimonialsDialogJSONTab
-              v-show="currentActiveTab === 'JSON'"
+              v-show="activeTab === 'JSON'"
               v-model:testimonials="testimonials"
             />
           </main>
@@ -52,18 +61,18 @@
 
         <!-- Dialog footer -->
         <footer>
-          <menu class="dialog__footer">
+          <menu class="dialog-content__footer">
             <button
               autofocus
               type="reset"
-              class="dialog__footer-cancel"
+              class="dialog-content__footer-cancel"
               @click.prevent="closeDialog('cancel')"
             >
               Cancel
             </button>
             <button
               type="submit"
-              class="dialog__footer-confirm"
+              class="dialog-content__footer-confirm"
               value="confirm"
             >
               Update testimonials
@@ -91,7 +100,7 @@ const { getTestimonials } = useData();
 const dialogEl = ref<HTMLDialogElement | null>(null);
 
 // Keeps track of which tab is active. Default is HTML.
-const currentActiveTab = ref<string>('HTML');
+const activeTab = ref<string>('HTML');
 
 // List of testimonials to edit.
 const testimonials = ref<ITestimonial[]>([]);
@@ -128,7 +137,7 @@ const closeDialog = (action: 'close' | 'cancel'): void => {
  * @author Brian Kariuki <bkariuki@hotmail.com>
  */
 const handleTabClick = (tab: 'HTML' | 'JSON'): void => {
-  currentActiveTab.value = tab;
+  activeTab.value = tab;
 };
 
 // When the component is mounted, fetch the testimonials.
@@ -141,18 +150,41 @@ onMounted(async () => {
 </script>
 
 <style scoped>
-#testimonials {
+button {
+  border: 2px solid transparent;
+  border-radius: 8px;
+
+  padding: 0.7rem 1.4rem;
+
+  font-size: 0.9rem;
+  font-weight: 400;
+
+  transition: all 0.2s ease-in;
+  cursor: pointer;
+}
+
+button:focus {
+  outline: none;
+}
+
+.dialog-trigger {
+  background-color: #1d4ed8;
+
+  color: #eff6ff;
+}
+
+.dialog-content {
   border: none;
   border-radius: 18px;
 
-  box-shadow: 0px 5px 20px 10px rgba(0, 0, 0, 0.1);
+  box-shadow: 0px 2px 25px 4px rgba(0, 0, 0, 0.1);
 
   padding: 0;
 
   transition: opacity 0.5s ease-out;
 }
 
-.dialog__header {
+.dialog-content__header {
   box-shadow: 0px 7px 25px 2px rgba(0, 0, 0, 0.1);
 
   padding: 1.6rem 2rem;
@@ -164,13 +196,13 @@ onMounted(async () => {
   font-size: 1.4rem;
 }
 
-.dialog__header h3 {
+.dialog-content__header h3 {
   margin: 0;
 }
 
-.dialog__header button {
+.dialog-content__header button {
   width: 45px;
-  height: 40px;
+  height: 45px;
 
   outline: none;
   border: none;
@@ -185,16 +217,43 @@ onMounted(async () => {
   transition: background 0.2s ease-in;
 }
 
-.dialog__header button:hover {
+.dialog-content__header button:hover {
   background-color: #e2e8f0;
 }
 
-.dialog__content {
+.dialog-content__body {
+  height: 50vh;
+
+  overflow-y: auto;
+
   margin: 0;
-  padding: 1.6rem 2rem;
+  padding: 1.2rem 2rem;
 }
 
-.dialog__footer {
+.dialog-content__body__nav {
+  margin-bottom: 1rem;
+}
+
+.dialog-content__body__nav-item {
+  border-radius: 0;
+
+  background-color: transparent;
+}
+
+.dialog-content__body__nav-item:hover {
+  border-radius: 0;
+  border-top-left-radius: 4px;
+  border-top-right-radius: 4px;
+
+  background-color: #e2e8f0;
+}
+
+.dialog-content__body__nav-item.active {
+  border-radius: 0;
+  border-bottom: 2px solid #2563eb;
+}
+
+.dialog-content__footer {
   box-shadow: 0px -7px 25px 2px rgba(0, 0, 0, 0.1);
 
   margin: 0;
@@ -205,35 +264,22 @@ onMounted(async () => {
   align-items: center;
 }
 
-.dialog__footer button {
-  border: 2px solid transparent;
-  border-radius: 8px;
-
-  padding: 0.75rem 1.2rem;
-
-  font-size: 0.9rem;
-  font-weight: 400;
-
-  transition: all 0.2s ease-in;
-  cursor: pointer;
-}
-
-.dialog__footer-cancel {
+.dialog-content__footer-cancel {
   background: none;
 
   color: #334155;
 }
 
-.dialog__footer-cancel:hover {
+.dialog-content__footer-cancel:hover {
   background: #e2e8f0;
   border: 2px solid #e2e8f0;
 }
 
-.dialog__footer-cancel:focus {
+.dialog-content__footer-cancel:focus {
   border: 2px solid #cbd5e1;
 }
 
-.dialog__footer-confirm {
+.dialog-content__footer-confirm {
   border: 2px solid #1d4ed8;
   background: #1d4ed8;
 
